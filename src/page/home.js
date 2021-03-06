91a53c04dc360dab0fe3b866e76aefdcc7379dec
afifpratama <afifpratama@gmail.com>
2021-03-06 16:03:08 +0700

    bener

--- src/page/home.js
+++ src/page/home.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
-import MyBottomNavigation from '../component/bottomnav';
-import Countdown from 'react-countdown';
-import tes from "./homepage.png"
-
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-    return <h1>{days} hari lagi Hours = {hours}, minute = {minutes}:{seconds}</h1>;
-};
 
 class Home extends Component {
     state = {}
     render() {
         return (
-            <div style={{ backgroundImage: `url("https://cdn.discordapp.com/attachments/776072772685398016/817632537090195456/Tes_Homepage.png")})` }}>
-                <Countdown date="2021-06-01" renderer={renderer} />
+            <div style={{
+                backgroundImage: `url(https://cdn.discordapp.com/attachments/776072772685398016/817632537090195456/Tes_Homepage.png)`,
+
+            }
+            }>
                 hello
-            </div>);
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd                hello
+                <br></br>
+                asd
+            </div >);
     }
 }
 
