d4e9e4886cf0b6ee1881194e093580df7337d984
afifpratama <afifpratama@gmail.com>
2021-03-08 13:02:30 +0700

    tes lagi

--- src/page/home.js
+++ src/page/home.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import foto from "./foto.png"
+import Countdown from 'react-countdown';
+
+const renderer = ({ days, hours, minutes }) => {
+    return <h1>{days}&nbsp; {hours}&nbsp; &nbsp; {minutes}</h1>;
+};
 
 class Home extends Component {
     state = {}
     render() {
         return (
-            <div >
-                tsdas
-                <div style={{
-                    backgroundImage: `url(https://cdn.discordapp.com/attachments/776072772685398016/817632537090195456/Tes_Homepage.png)`,
-                    padding: 10
-                }}>
-                    asd
+            <div style={{
+                backgroundImage: `url(${foto})`,
+                position: "absolute", width: "100%", height: "100%", backgroundSize: "100%"
+            }}>
+                <div style={{ left: "40%", position: "absolute", top: "45%", fontfamily: 'Arial', color: "#fff", fontSize: "30px" }}>
+                    Undangan Pernikahan
+                </div>
+                <div style={{ left: "32%", position: "absolute", top: "48%", fontfamily: 'Arial', color: "#fff", fontSize: "100px" }}>
+                    Fia &amp; Edgar
+                </div>
+                <div style={{ left: "42%", position: "absolute", top: "70%", fontfamily: 'Arial', color: "#fff", fontSize: "30px" }}>
+                    27 juni 2021
+                </div>
+                <div style={{ left: "42%", position: "absolute", top: "80%", fontfamily: 'Arial', color: "#fff", fontSize: "30px" }}>
+                    <Countdown date="2021-06-27" renderer={renderer} />
+                </div>
+                <div style={{ left: "42%", position: "absolute", top: "85%", fontfamily: 'Arial', color: "#fff", fontSize: "30px" }}>
+                    Days          Hours        minutes
                 </div>
             </div >);
     }
--- src/page/tesHomepage.jsx
+++ src/page/tesHomepage.jsx
@@ -17,11 +17,9 @@ class TesHomepage extends React.Component {
 
     return (
       <div data-layer="a45470a5-0861-41cf-a649-3fe99e486fea" className="tesHomepage">        <div data-layer="02b48cc1-0862-483e-91cf-243d957b07cf" className="rectangle3"></div>
-        <div data-layer="a1e826ca-3a3e-4f6a-a42c-4f3234a9b716" className="edgar">Edgar</div>
-        <div data-layer="95f35eb1-12a1-40b7-94a0-1b0867995d4b" className="fia">Fia</div>
+        <div data-layer="95f35eb1-12a1-40b7-94a0-1b0867995d4b" className="fia">Fia &amp; Edgar</div>
         <div data-layer="4e0802e4-76c2-4ecb-9260-bfe9db31ca59" className="x27Juni2021">27 Juni 2021</div>
         <div data-layer="2b998c20-99df-4d75-8ef4-0d29b608632d" className="undanganPernikahan">Undangan Pernikahan</div>
-        <div data-layer="c14c8a15-60ec-4381-8f9f-88cfeecd9e33" className="x6d5e094f">&</div>
         <svg data-layer="23e254af-024d-45e7-a964-e8377d349476" preserveAspectRatio="none" viewBox="-0.00823974609375 -0.99993896484375 879.7824096679688 9.23541259765625" className="line1"><path d="M 0 0 L 879.7659912109375 7.23553466796875" /></svg>
         <svg data-layer="bc1b7dce-aa80-43f9-9c66-e6c5f69aef52" preserveAspectRatio="none" viewBox="-0.00848388671875 -0.999969482421875 880.2510375976562 9.44940185546875" className="line2"><path d="M 0 0 L 880.2340087890625 7.449462890625" /></svg>
         <div data-layer="44b406e1-252a-40c9-a425-ee7237469926" className="jek6412"></div>
